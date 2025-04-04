98dd27147d13cd725687332d9c8175e1f3a56f3f
Gustavo_lima07 <luis.lima133@etec.sp.gov.br>
2025-04-04 09:59:05 -0300

    Projeto criado

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './components'
+import Footer from './components/Footer/Footer.jsx'
+import { Inicial }  from './pages/Inicial/Inicial.jsx'
+
+const  App = () => {
+  return (
+    <div>
+      
+      <Inicial></Inicial>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import style from './Footer.module.css';
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer className={style.footer}>
+            <>React - 2025 - Luis Gustavo</>
+        </footer>
+    );
+}
+
+export default Footer;
