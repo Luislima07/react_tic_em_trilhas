b9997ba3974b412107a87fde19f1c573aa721d0a
Gustavo_lima07 <luis.lima133@etec.sp.gov.br>
2025-04-10 15:08:12 -0300

    Api adicionada ao projeto
    Api local que salva o que usuário crie no site

--- vite-project/src/context/AppContext.jsx
+++ vite-project/src/context/AppContext.jsx
@@ -1,48 +1,59 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
 export const AppContext = createContext({});
 
 export const AppContextProvider = (props) => {
     const { children } = props;
 
+    const [tarefas, setTarefas] = useState([]);
+    
+    const carregarTarefas = async () => {
+        const { data = [ ]} = await api.get('/tarefas');
 
+        setTarefas([
+            ...data,
+        ])
+    };  
 
-    const [tarefas, setTarefas] = useState ([
-        { id: 1, nome: "Tarefa 1" },
-        { id: 2, nome: "Tarefa 2" },
-        { id: 3, nome: "Tarefa 3" },
-    ]);
-
-    const adicionarTarefa = (nomeTarefa) => {
+    const adicionarTarefa = async (nomeTarefa) => {
+        const { data: tarefa } = await api.post('/tarefas', {
+            nome: nomeTarefa, 
+        });
+        
         setTarefas(estadoAtual => {
-            const tarefa = {
-                id: estadoAtual.length + 1,
-                nome: nomeTarefa
-            }
             return [...estadoAtual, tarefa];
         });
     };
 
-    const removerTarefa = (idTarefa) => {
+    const removerTarefa = async (idTarefa) => {
+        await api.delete(`tarefas/${idTarefa}`);
+
         setTarefas(estadoAtual => {
             const tarefasAtualizadas = estadoAtual.filter(tarefa => tarefa.id != idTarefa);
 
             return [
                 ...tarefasAtualizadas
             ];
-        })
-    }
+        });
+    };
 
-    const editarTarefa = (idTarefa, nomeTarefa) => {
+    const editarTarefa = async (idTarefa, nomeTarefa) => {
+        const { data: tarefasAtualizada } = await api.put(`tarefas/${idTarefa}`, {
+            nome: nomeTarefa,
+        });
+
         setTarefas(estadoAtual => {
             const tarefasAtualizadas = estadoAtual.map(tarefa => 
-                tarefa.id === idTarefa ? { ...tarefa, nome: nomeTarefa } : tarefa
+                tarefa.id === idTarefa ? { ...tarefa, nome: tarefasAtualizada.nome } : tarefa
             );
             return [...tarefasAtualizadas];
         });
     };
         
-
+    useEffect(() => { 
+        carregarTarefas();
+    }, []);
     
     return (
         <AppContext.Provider value={{
